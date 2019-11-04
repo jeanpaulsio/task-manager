0b2c0492eef61f5b28c6a015115712ad5e9b606d
JP Sio <sio.jeanpaul@gmail.com>
2019-11-03 22:34:37 -0800

    Finishes crud changes'

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,22 @@ import "./App.css";
 function App() {
   const [data, setData] = React.useState([]);
   const [newTask, setNewTask] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("/tasks");
-      setData(result.data);
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios("/tasks");
+        setData(result.data);
+      } catch (e) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
     };
     fetchData();
   }, []);
@@ -20,15 +31,41 @@ function App() {
       <ul>
         {tasks.map(task => (
           <li key={task._id}>
-            <div>
-              <input type="checkbox" />
+            <div className="pointer" onClick={() => handleToggleTask(task._id, !task.completed)}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => handleToggleTask(task._id, !task.completed)}
+              />
               {task.description}
             </div>
-            <button>Delete</button>
+            <button onClick={() => handleDeleteTask(task._id)}>Delete</button>
           </li>
         ))}
       </ul>
     );
+  }
+
+  async function handleToggleTask(id, completed) {
+    try {
+      await axios.patch(`/tasks/${id}`, { completed });
+
+      // Optimistically render changes
+      setData(prevData => prevData.map(task => (task._id === id ? { ...task, completed } : task)));
+    } catch (e) {
+      //
+    }
+  }
+
+  async function handleDeleteTask(id) {
+    try {
+      await axios.delete(`/tasks/${id}`);
+
+      // Optimistically render changes
+      setData(prevData => prevData.filter(task => task._id !== id));
+    } catch (e) {
+      //
+    }
   }
 
   async function handleAddTask(e) {
@@ -41,7 +78,7 @@ function App() {
       setData(prevData => [...prevData, data]);
       setNewTask("");
     } catch (e) {
-      //
+      setIsError(true);
     }
   }
 
@@ -49,7 +86,9 @@ function App() {
     <div className="App">
       <section className="App-body">
         <h1>Tasks</h1>
-        {data.lenth === 0 ? <p>No Tasks</p> : renderTasks(data)}
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Something went wrong...</p>}
+        {data.length > 0 && renderTasks(data)}
         <form onSubmit={handleAddTask}>
           <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
         </form>
