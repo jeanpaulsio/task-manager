19e5c1bd8712967939398207e68b7cd3d5c3e077
JP Sio <sio.jeanpaul@gmail.com>
2019-11-03 22:17:13 -0800

    Starts displaying ui

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,56 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  // const [data, setData] = React.useState([]);
+  const [data, setData] = React.useState([]);
+  const [newTask, setNewTask] = React.useState("");
 
-  // React.useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios("/tasks");
-  //     setData(result.data);
-  //   };
-  //   fetchData();
-  // }, []);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("/tasks");
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
 
-  // console.log(data);
+  function renderTasks(tasks) {
+    return (
+      <ul>
+        {tasks.map(task => (
+          <li key={task._id}>
+            <div>
+              <input type="checkbox" />
+              {task.description}
+            </div>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  async function handleAddTask(e) {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.post("/tasks", {
+        description: newTask,
+      });
+      setData(prevData => [...prevData, data]);
+      setNewTask("");
+    } catch (e) {
+      //
+    }
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <section className="App-body">
+        <h1>Tasks</h1>
+        {data.lenth === 0 ? <p>No Tasks</p> : renderTasks(data)}
+        <form onSubmit={handleAddTask}>
+          <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
+        </form>
+      </section>
     </div>
   );
 }
